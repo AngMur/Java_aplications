d9c0146af4b903494f8ef33a58210a160fb4e17b
Angel_Muratalla <angelgarcia80@aragon.unam.mx>
2024-02-17 13:30:54 -0600

    Calculadora version final

--- Clase1_Calculadora/Calculadora.java
+++ Clase1_Calculadora/Calculadora.java
@@ -1,60 +1,133 @@
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 import java.awt.*;
+import static java.awt.Font.PLAIN;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 public class Calculadora extends JFrame {
 
-    JLabel display;
-    JPanel buttons;
-    String textButtons[] = 
-            {"7", "8", "9", "+",
-            "4", "5", "6", "-",
-            "3", "2", "1", "*",
-            "0", ".", "=", "/"};
-
-    public  Calculadora(){
-        this.setLayout(null); // Dispocision absoluta de los componenetes (Ponerlos como queramos)
-        this.setTitle("Calculadora"); // Titulo ventana
-        this.setSize(400,450); // Tamaño ventana
-        this.setLocationRelativeTo(null); // Centrado ventana
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE); 
-        // this.setResizable(false); // No cambiar su tamaño
-
-        initComponets();
-
-        this.setVisible(true); // Hacer visible la ventana
+    public static void main(String[] args) {
+        new Calculadora();
     }
 
-    private void initComponets(){
-        initDisplay();
-        initButtons();
+    //Display para mostrar las expresiones
+    JLabel display;
+    //Array de strings para las etiquetas de los botones
+    String textButtons[] = {"=","7","8","9","/","4","5","6","*","1","2","3","-","C","0",".","+"};
+    //Array de botones para números y operaciones
+    JButton botones[] = new JButton[textButtons.length];
+    //Array de posiciones en X de cada botón
+    int x[] = {15, 15, 80, 145, 210, 15, 80, 145, 210, 15, 80, 145, 210, 15, 80, 145, 210};
+    //Array de posiciones en Y de cada botón
+    int y[] = {90, 155, 155, 155, 155, 220, 220, 220, 220, 285, 285, 285, 285, 350, 350, 350, 350};
+    
+    public Calculadora(){
+        //Caracteristicas de la ventana
+        this.setResizable(false);
+        this.setTitle("Calculadora");
+        this.setBounds(0, 0, 290, 455);
+        this.setLocationRelativeTo(null);
+        this.setLayout(null);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        //Inicialización de los componentes
+        this.initComponents();
+
+        //Ajustando visibilidad de la ventana
+        this.setVisible(true);
+    }
+
+    private void initComponents(){
+        this.initDisplay();
+        this.initButtons();
     }
 
     private void initDisplay(){
-        display = new JLabel("0"); // Inicio JLabel
-        display.setBounds(15,15, this.getWidth() - 50, 60); // Posición y dimensiones
-        display.setOpaque(true); // Para poder darle un color de fondo
-        display.setBackground(Color.BLACK); // Color de fondo
-        display.setForeground(Color.WHITE); // Color de fuente
-        display.setHorizontalAlignment(SwingConstants.RIGHT); // Alineamiento horizontal derecha
-        this.add(display);
+        display = new JLabel("0"); //Inicio JLabel
+        display.setBounds(15, 15, 245, 60); //Posición y dimensiones
+        display.setOpaque(true); //Para poder darle un color de fondo
+        display.setBackground(Color.BLACK); //Color de fondo
+        display.setForeground(Color.WHITE); //Color de fuente
+        display.setBorder(new LineBorder(Color.GRAY)); //Borde
+        display.setFont(new Font("MONOSPACED", PLAIN, 24)); //Fuente
+        display.setHorizontalAlignment(SwingConstants.RIGHT); //Alineamiento horizontal derecha
+        this.add(display); //Añado el JLabel al JFrame
     }
 
     private void initButtons(){
-        buttons = new JPanel();
-        buttons.setBounds(15, 80, this.getWidth() - 50, this.getHeight() - 150);
-        buttons.setBackground(Color.BLACK);
+        for (int i = 0; i < textButtons.length; i++){
 
-        buttons.setLayout(new GridLayout(4,4));
-        for (int i = 0; i < textButtons.length; i++) {
-            buttons.add(new Button(textButtons[i]));
+            JButton button = new JButton(textButtons[i]); //Inicializo JButton
+            int fontSize = (i == 0) ? 24 : 16; //EL botón de Resultado tendrá un tamaño de fuente menor que todos los demás
+            int width = (i == 0) ? 245 : 50; //EL botón de Resultado será más ancho que todos los demás
+            int heigth = 50; //Todos los botones tienen la misma altura
+
+            button.setBounds(x[i], y[i], width, heigth); //Posición y dimensiones
+            button.setFont(new Font("MONOSPACED",PLAIN, fontSize)); //Fuente
+            button.setOpaque(true); //Para poder darle un color de fondo
+            button.setFocusPainted(false); //Para que no salga una recuadro azul cuando tenga el foco
+            button.setBackground(Color.DARK_GRAY); //Color de fondo
+            button.setForeground(Color.WHITE); //Color de fuente
+            button.setBorder(new LineBorder(Color.DARK_GRAY)); //Borde
+            //Eventos
+            if(textButtons[i].contentEquals("=")){
+                button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        // Acción a realizar cuando se hace clic en el botón
+                        
+                        // Crear un gestor de motores de script
+                        ScriptEngineManager manager = new ScriptEngineManager();
+                        // Obtener un motor de script
+                        ScriptEngine engine = manager.getEngineByName("JavaScript");
+
+                        try {
+                            // Evaluar la expresión y mostrar el resultado
+                            Object result = engine.eval(display.getText());
+                            display.setText(result + "");
+                        } catch (ScriptException error) {
+                            display.setText("Syntax ERROR");
+                        }
+                    }
+                });
+            } else if(textButtons[i].contentEquals("C")){
+                button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        // Acción a realizar cuando se hace clic en el botón
+                        display.setText("0");
+                    }
+                });
+            }else{
+                button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        // Acción a realizar cuando se hace clic en el botón
+                        addText(button.getText());
+                    }
+                });
+            }
+
+            this.add(button); //Añado el JButton al JFrame
         }
-
-        this.add(buttons);
     }
 
-    public static void main(String[] args) {
-        Calculadora c = new Calculadora();
-        c.setVisible(true);
+    private void addText(String s){
+        if(display.getText().contentEquals("0")){
+            display.setText(s);
+        } else if(display.getText().contentEquals("Syntax ERROR")){
+            display.setText(s);
+        }else{
+            display.setText(display.getText() + s);
+        }
     }
 
 }
